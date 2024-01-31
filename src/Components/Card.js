884f9ea8008bd588cdfb8da85be5a28fcb6c1643
Misbah Fatma <misbah7370@gmail.com>
2024-02-01 01:35:08 +0530

    first commit

---
+++ src/Components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import '../styles/Card.css'
+const Card = (props) => {
+    const navigate = useNavigate();
+    const viewDetailsBtnClicked = () => {
+        navigate('/item_details', { state: props });
+    }
+    return (
+        <div className="scene">
+        <div className="card">
+            <img src={props.val.show.image?.original} className="card-img-top" alt="..." />
+            <div className="middle">
+                <button className="button-74" onClick={viewDetailsBtnClicked}>View Details </button>
+            </div>
+            <div className="card-body">
+                <h5 className="card-title">{props.val.show.name}</h5>
+                <p className="rating mt-3">
+                    <b>Rating : </b>{props.val.show.rating.average}
+                </p>
+            </div>
+            </div>
+        </div >
+    )
+}
+
+export default Card
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="container-fluid footerDiv">
+           
+          <p>Assignment for Quad B Tech <em>By Misbah Fatma</em></p>
+        </div>
+    )
+}
+
+export default Footer
